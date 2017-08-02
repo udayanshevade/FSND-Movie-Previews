4ec8aae8b71676b8844ddeb4dda176b9deb25ac7
Udayan Shevade <udayan.shevade@gmail.com>
2017-08-02 00:23:31 -0700

    add shuffle button, adjust appearance

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,7 @@ $(function() {
     shift: 50,
     padding: 150,
   };
+  var NUM_MOVIES = 7;
   var carouselInitialized;
 
   /**
@@ -34,6 +35,8 @@ $(function() {
     // Adjust height before initializing carousel
     setCarouselHeight();
     $('.carousel').carousel(options);
+    // sample animation on load
+    $('.carousel').carousel('set', Math.round($('.carousel-item').length / 2));
   }
 
   // Initialize modal and add listeners
@@ -49,14 +52,17 @@ $(function() {
 
     // Start playing the video whenever the trailer modal is opened
     $('.modal-trigger').click(function (event) {
-        var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-        var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-        $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-          'id': 'trailer-video',
-          'type': 'text-html',
-          'src': sourceUrl,
-          'frameborder': 0
-        }));
+        // prevent accidental video trigger on unselected carousel items
+        if ($(this).parent('.carousel-item').hasClass('active')) {
+          var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+          var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+          $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+            'id': 'trailer-video',
+            'type': 'text-html',
+            'src': sourceUrl,
+            'frameborder': 0
+          }));
+        }
     });
 
     $(document).keyup(function(event) {
@@ -67,12 +73,19 @@ $(function() {
     });
   }
 
+  // Add click listener for randomizer button
+  function initializeShuffler() {
+    $('.randomizer').click(function() {
+      var newNum = Math.floor(Math.random() * NUM_MOVIES);
+      $('.carousel').carousel('set', newNum);
+    });
+  }
+
   initializeModal();
   initializeCarousel();
   // If browser is resized, set carousel height again
   $(document).on('resize', function() {
     initializeCarousel(CAROUSEL_OPTIONS);
   });
-  // sample animation on load
-  $('.carousel').carousel('set', Math.round($('.carousel-item').length / 2));
+  initializeShuffler();
 });
